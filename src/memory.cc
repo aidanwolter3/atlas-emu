f02cbdd39967ca40c09ab98b6a9347705abb5033
Aidan Wolter <wolter.aidan@gmail.com>
2020-05-06 20:48:06 -0700

    Add empty Memory class
    This class will receive the raw data form the ROM, and allow other
    classes to read/write.

---
+++ src/memory.cc
@@ -0,0 +1,14 @@
+#include "memory.h"
+
+Memory::Memory(std::vector<uint8_t> data)
+    : data_(std::move(data)) {}
+
+Memory::~Memory() = default;
+
+uint8_t Memory::Read(uint16_t address) {
+  return data_[offset];
+}
+
+uint8_t Memory::Write(uint16_t address) {
+  return 0;
+}
---
+++ src/memory.h
@@ -0,0 +1,19 @@
+#ifndef MEMORY_H_
+#define MEMORY_H_
+
+#include <cstdint>
+#include <vector>
+
+class Memory {
+ public:
+  Memory(std::vector<uint8_t> data);
+  ~Memory();
+
+  uint8_t Read(uint16_t offset);
+  uint8_t Write(uint16_t offset);
+
+ private:
+  std::vector<uint8_t> data_;
+};
+
+#endif  // MEMORY_H_
