e7aa78f07be93c7fdd660064ec95469a74768960
Aidan Wolter <wolter.aidan@gmail.com>
2020-11-21 14:10:45 -0800

    Add branching instructions and simple integration test

--- src/atlas_test.cc
+++ src/atlas_test.cc
@@ -12,3 +12,8 @@ TEST(AtlasTest, PrettyLog) {
   Atlas atlas("src/roms/pretty_log_test.nes");
   EXPECT_EQ(atlas.RunTimes(7), Cpu::Status::OK);
 }
+
+TEST(AtlasTest, Branch) {
+  Atlas atlas("src/roms/branch_test.nes");
+  EXPECT_EQ(atlas.RunTimes(13), Cpu::Status::OK);
+}
---
+++ src/roms/branch_test.s
@@ -0,0 +1,21 @@
+.segment "HEADER"
+  .byte 'N', 'E', 'S', $1a
+  .byte 2 ; number of 16k PRG ROM segments
+  .byte 0 ; number of 8k CHR ROM segments
+
+.segment "CODE"
+  main:
+    nop
+    nop
+    nop
+    nop
+    lda #$00
+    beq main
+
+.segment "VECTORS"
+  .word main ; $fffa = nmi
+  .word main ; $fffc = reset
+
+.segment "STARTUP"
+.segment "CHARS"
+.segment "BSS"
